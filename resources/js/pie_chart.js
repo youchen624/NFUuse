ff08ae0a119908e9ff270bb32accd7d6e99d3749
youchen624 <youchen624@gmail.com>
2024-10-21 11:49:22 +0800

    1

--- resources/js/pie_chart.js
+++ resources/js/pie_chart.js
@@ -1,14 +1,13 @@
-// 獲取點擊次數，若不存在則初始化
+
 let button1Count = parseInt(localStorage.getItem('button1Count')) || 1;
 let button2Count = parseInt(localStorage.getItem('button2Count')) || 1;
 let button3Count = parseInt(localStorage.getItem('button3Count')) || 1;
 
-// 初始化圖表
 const ctx = document.getElementById('myPieChart').getContext('2d');
 const myPieChart = new Chart(ctx, {
     type: 'pie',
     data: {
-        labels: ['品項1', '品項2', '品項3'],
+        labels: ['項目1', '項目2', '項目3'],
         datasets: [{
             data: [button1Count, button2Count, button3Count],
             backgroundColor: ['#36a2eb', '#ff6384', '#ffce56']
@@ -20,7 +19,6 @@ const myPieChart = new Chart(ctx, {
     }
 });
 
-// 增加按鈕點擊事件
 document.getElementById('button1').addEventListener('click', function() {
     button1Count++;
     localStorage.setItem('button1Count', button1Count);
@@ -39,7 +37,6 @@ document.getElementById('button3').addEventListener('click', function() {
     updateChart();
 });
 
-// 減少按鈕點擊事件
 document.getElementById('button1Decrease').addEventListener('click', function() {
     if (button1Count > 0) {
         button1Count--;
@@ -64,22 +61,17 @@ document.getElementById('button3Decrease').addEventListener('click', function() 
     }
 });
 
-// 重置按鈕事件
 document.getElementById('resetButton').addEventListener('click', function() {
-    // 將所有計數歸零
     button1Count = 1;
     button2Count = 1;
     button3Count = 1;
-    // 更新 Local Storage
     localStorage.setItem('button1Count', button1Count);
     localStorage.setItem('button2Count', button2Count);
     localStorage.setItem('button3Count', button3Count);
-    // 更新圖表
     updateChart();
 });
 
-// 更新圓餅圖
 function updateChart() {
     myPieChart.data.datasets[0].data = [button1Count, button2Count, button3Count];
-    myPieChart.update(); // 更新圖表
+    myPieChart.update();
 }
